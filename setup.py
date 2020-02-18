e39a0ac2f9d91c9917f4dfd2954abf85540f225a
K Lauer <klauer@slac.stanford.edu>
2020-02-18 15:47:29 -0800

    MNT: change binary to tc-release, because we can

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'tc_release = tc_release.tc_release:main'
+            'tc-release = tc_release.tc_release:main'
         ]
     }
   )
