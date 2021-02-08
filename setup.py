a1e3c6ef973da7e0d70056161d4e34378f07ee75
Zachary Lentz <zlentz@slac.stanford.edu>
2021-02-08 15:08:51 -0800

    MNT: update setup.py with an automatic include of the required manifest components

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup, find_packages
-import versioneer
+from setuptools import find_packages, setup
 
+import versioneer
 
 with open('requirements.txt', 'rt') as f:
     requirements = f.read().splitlines()
 
 requirements = [r for r in requirements if not r.startswith('git+')]
+
+# package_data is required for source distributions
+with open('MANIFEST.in', 'rt') as f:
+    manifest = f.read().splitlines()
+
+# Remove the "include" from the manifest to get filenames
+files = [m.split(' ')[1] for m in manifest]
+# Only include contents of tc_release folder
+files = [f.split('/')[1] for f in files if 'tc_release' in f]
+package_data = {'tc_release': files}
 
 setup(
     name='tc_release',
@@ -14,9 +24,10 @@ setup(
     license='BSD',
     author='SLAC National Accelerator Laboratory',
     install_requires=requirements,
-    packages= find_packages(),
+    packages=find_packages(),
     description='A TwinCAT project release tool',
     include_package_data=True,
+    package_data=package_data,
     entry_points={
         'console_scripts': [
             'tc-release = tc_release.tc_release:main'
